f940f101837ce6b5ce3f6501ad784d4c2cc49484
长生 <jacke.yang@uccvr.com>
2024-09-06 14:02:44 +0800

    更新脚本

---
+++ ApplicationServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
